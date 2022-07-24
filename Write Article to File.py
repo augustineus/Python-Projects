0188a47eda8f7f58ea430532d56b9ad60b59289d
Tim <timortega95@gmail.com>
2022-07-24 12:08:09 -0600

    instructions, updated URL

--- Write Article to File.py
+++ Write Article to File.py
@@ -1,15 +1,17 @@
+#change base_url to the site you want to scrape
+#change text_to_find's class_='' to the HTML class of the site
+#this will return a text file with many lists, all with one string containing the text of the HTML class(es) chosen
+
 import requests
 from bs4 import BeautifulSoup
 
-base_url = 'http://www.nytimes.com'
+base_url = 'https://www.theguardian.com/us'
 r = requests.get(base_url)
 soup = BeautifulSoup(r.text)
 
-filename = input("File to save to: ")
+filename = input('Save file as: ')
 
 with open(filename, 'w') as f:
-  for story_heading in soup.find_all(class_="story-heading"):
-      if story_heading.a:
-          f.write(story_heading.a.text.replace("\n", " ").strip())
-      else:
-          f.write(story_heading.contents[0].strip())
+    for text_to_find in soup.find_all(class_="u-faux-block-link__overlay js-headline-text"):
+        newSoup = str(text_to_find.contents)
+        f.write(''.join(newSoup))
