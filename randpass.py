a5603274971bca7bb572478ae2af3b8e9a2e594b
augustineus <timortega95@gmail.com>
2022-07-24 11:11:17 -0600

    updated to meet modern pw requirements

--- randpass.py
+++ randpass.py
@@ -1,12 +1,27 @@
 import random
 import string
 
-a = int(input('How long do you want your password to be?: '))
+a = int(input('How many characters do you need your password to be?: '))
+b = int(input('How many numbers do you want in it?: '))
+c = int(input('How many special characters do you want in it?: '))
+d = a - b - c
 pw = ''
 while len(pw) < a:
-    pw += random.choice(string.ascii_letters)
-    pw += random.choice(string.digits)
-    pw += random.choice(string.punctuation)
-    if len(pw) == a - 1:
+    while d > 0:
+        pw += random.choice(string.ascii_letters)
+        d -= 1
+    if len(pw) == a:
         break
-print(''.join(random.sample(pw, len(pw))))
+    while b > 0:
+        pw += random.choice(string.digits)
+        b -= 1
+    if len(pw) == a:
+        break
+    while c > 0:
+        pw += random.choice('!@#$%^&*')
+        c -= 1
+    if len(pw) == a:
+        break
+newPW = ''.join(random.sample(pw, len(pw)))
+
+print(newPW.capitalize())
